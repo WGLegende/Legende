447cc2fa9fc559041c1f6010062f1ef940278665
CharlyRaclett <56432296+ugogrange@users.noreply.github.com>
2020-04-11 11:00:03 +0200

    modif collider chariot

--- Legende_unity/Assets/Scripts/ChariotPlayer.cs
+++ Legende_unity/Assets/Scripts/ChariotPlayer.cs
@@ -196,7 +196,8 @@ public class ChariotPlayer : MonoBehaviour
         //             if (Input.GetKey(i))
         //             Debug.Log(i);
         //     }     
-    }
+    
+    } 
 
    
     IEnumerator refreshSpeedUI(){
