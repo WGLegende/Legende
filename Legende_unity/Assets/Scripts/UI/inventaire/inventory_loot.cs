e5392d02b033e22d5faf0e633d126795c58009ab
Ugo <ugo.grange@actived.fr>
2020-06-28 20:48:46 +0200

    [objects] Lootable V1

--- Legende_unity/Assets/Scripts/UI/inventaire/inventory_loot.cs
+++ Legende_unity/Assets/Scripts/UI/inventaire/inventory_loot.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 
 public class inventory_loot : MonoBehaviour
 {
-    [Serializable]
-    public struct RandomLoot{
-        public GameObject loot_object;
-        public int loot_percentage;
-    };
-    public RandomLoot[] _RandomLoot;
+    public inventory_object[] loot_objects;
+    public int[] loot_percentage = new int[100];
+    public int[] loot_quantite_min = new int[100];
+    public int[] loot_quantite_max = new int[100];
+
+    void Awake(){
+        loot_objects = GetComponentsInChildren<inventory_object>();
+        foreach (inventory_object obj in GetComponentsInChildren<inventory_object>())
+        {
+            obj.gameObject.SetActive(false);   
+        }
+
+        // Remove 3D if loot is on an enemy
+        if(GetComponentsInParent<enemy>().Length > 0){
+            Destroy (GetComponent<MeshRenderer>());
+            Destroy (GetComponent<MeshFilter>());
+        }
+    }
 
 
+    void OnTriggerEnter(Collider collider)
+    {
+        
+        // Faire ici la logique pour afficher ce qu'il y a a l'interieur...
 
+    }
 
 
 
